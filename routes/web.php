29510375d897fcedd406a9bd95720db612de0c82
kevin g <34563281+jennatools-alt@users.noreply.github.com>
2022-03-08 12:37:12 -0800

    added routes to this php file

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('todos', function () {
+    return view('todos');
+});
+
+Route::get('schedule', function () {
+    return view('calendar');
+});
+
+Route::get('board', function () {
+    return view('board');
+});
+
+
+
+
 URL::forceScheme('https');
