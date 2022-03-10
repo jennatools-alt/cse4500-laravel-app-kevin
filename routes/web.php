00f103b554a15051db0f4dc01ae6f04a6e9c6bbf
kevin g <34563281+jennatools-alt@users.noreply.github.com>
2022-03-10 09:35:34 -0800

    added a route for events-feed

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,19 @@ Route::get('/board', function () {
 
 URL::forceScheme('https');
 
-
+Route::get(events-feed/', function () {
+    return '[
+        {
+            "title": "CSE4500 Class",
+            "start": "2022-02-23T17:30:00",
+            "end": "2022-02-23T18:45:00"
+        },
+        {
+            "title": "CSE4500 Class",
+            "start": "2022-02-28T18:30:00",
+            "end": "2022-02-28T18:45:00",
+        }
+      ]';
+});
 
 
